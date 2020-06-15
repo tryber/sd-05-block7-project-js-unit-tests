2a745f012f5e37238128fd71ab0fa63ad5443e1f
Christian Duessel <duessel@gmail.com>
2020-06-15 17:18:48 -0300

    codeCLimateFIX2

--- src/average.js
+++ src/average.js
@@ -12,17 +12,17 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = (arraia) => { 
-  if (arraia.length === 0) { return undefined;}
+const average = (arraia) => {
+  if (arraia.length === 0) { return undefined; }
 
   let soma = 0;
   for (let i = 0; i < arraia.length; i += 1) {
     if (typeof arraia[i] !== 'number') {
       return undefined;
-    };
+    }
     soma += arraia[i];
   }
-  return (Math.round( soma / arraia.length ));
+  return (Math.round(soma / arraia.length));
 };
 
 // console.log(average([2, 3, 5]));
