f7354f394ffbc1a10ca6b52adffc792e2ccfdf0b
Lucas Costa Ventura <lucascv333@gmail.com>
2020-06-15 17:45:08 -0300

    corrigindo vqv

--- src/vqv.js
+++ src/vqv.js
@@ -21,7 +21,7 @@ const vqv = (nome, idade) => {
     return undefined;
   }
   return `Oi, meu nome é ${nome}!
-tenho ${idade} anos,
+Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
 };
