ad43a65fd9eabb459625d07d4c4c20436f28a754
Mau <mauricio.sdjr@gmail.com>
2020-06-15 16:54:38 -0300

    fix identation

--- src/average.js
+++ src/average.js
@@ -13,16 +13,16 @@
 */
 
 const average = (array) => {
-if (array.length === 0) {
+  if (array.length === 0) {
   return undefined;
-}
-let arrayAverage = 0;
-for (let i = 0; i < array.length; i += 1) {
-  if (typeof array[i] !== 'number') {
+  }
+  let arrayAverage = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
     return undefined;
   }
   arrayAverage += array[i] / array.length;
-}
+  }
 return Math.round(arrayAverage);
 };
 
