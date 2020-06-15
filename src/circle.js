a853c7d1ced63b5b20d56d51b7a0eb1885e66494
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-06-15 16:41:00 -0300

    teste 50000

--- src/circle.js
+++ src/circle.js
@@ -23,9 +23,11 @@ const circle = (radius) => {
   if (!radius) { return undefined; }
   return {
     radius,
-    area: PI * radius * radius,
+    area: parseFloat((PI * radius * radius).toFixed(2), 10),
     circumference: 2 * PI * radius,
   };
 };
 
 module.exports = circle;
+
+console.log(circle(3));
