5e90e429387e0e0478f04669d25d44f94a5c0cca
Preto115 <preto115@gmail.com>
2020-06-15 18:35:04 -0300

    implementacao do 3 requesito

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,7 +14,12 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
-
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const pessoa = {
+    name: nome,
+    feedback: () => ('Eita pessoa boa!'),
+  };
+  return pessoa;
+};
 
 module.exports = createStudent;
