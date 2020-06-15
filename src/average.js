6432daea5cc2d8c5dff88d057c68e2bb6f6ea74b
Mau <mauricio.sdjr@gmail.com>
2020-06-15 15:32:24 -0300

    codeclimate

--- src/average.js
+++ src/average.js
@@ -13,15 +13,15 @@
 */
 
 const average = (array) => {
-  if(array.length === 0) {
+  if (array.length === 0) {
     return undefined;
   }
   let arrayAverage = 0;
-  for(let i = 0; i < array.length; i += 1) {
-    if(typeof(array[i] !== "number")) {
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
       return undefined;
     }
-    arrayAverage += array[i]/array.length;
+    arrayAverage += array[i] / array.length;
   }
   return -Math.round(arrayAverage);
 };
