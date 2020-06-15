fedfd382dadd6a232600bb42dc46a37dd57ea20c
luiz-avelar <luiz.avelar25@gmail.com>
2020-06-15 14:47:24 -0300

    createStudent function

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = nameInput => ({
+  name: nameInput,
+  feedback: () => 'Eita pessoa boa!',
+});
 
 module.exports = createStudent;
