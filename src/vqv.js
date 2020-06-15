4fa9ca4fec494c7e8a234f8c2adc351f964ada39
Rafael Assad <rafaassadeng@gmail.com>
2020-06-15 16:56:18 -0300

    vqv final

--- src/vqv.js
+++ src/vqv.js
@@ -17,8 +17,13 @@
 */
 
 const vqv = (nome, idade) => {
-  const saudacao = 'Oi, meu nome é ' + nome + '! tenho ' + idade + ' anos, trabalho na Trybe e mando muito em programação! #VQV!';
-  return saudacao;
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 // console.log(vqv('roberto', 30))
 module.exports = vqv;
