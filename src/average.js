1544142e6f3983e0c94e35bf0a58fa938b098fe4
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-06-15 16:57:32 -0300

    correção 1 codeclimate

--- src/average.js
+++ src/average.js
@@ -14,13 +14,13 @@
 
 const average = (array) => {
   let sum = 0;
-  for(let i = 0; i < array.length; i += 1) {
-    if(typeof array[i] !== 'number') {
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
       return undefined;
     }
     sum += array[i];
   }
-  return Math.round(sum / array.length)
+  return Math.round(sum / array.length);
 };
 
 module.exports = average;
