168dd448ae8c26d100ca487f04f31bdf7795bb25
Isabela Rosa <isabelarosa954@gmail.com>
2020-06-15 16:29:02 -0300

    Iniciando o projeto

--- src/average.js
+++ src/average.js
@@ -12,6 +12,8 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = () => {
+  //adicionando um comentário de teste para o primeiro commit
+};
 
 module.exports = average;
