083a3e38e7401e407425a8ee55012055ecbb5788
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-06-15 15:17:03 -0300

    Corrigindo a função, passou nos testes.

--- src/average.js
+++ src/average.js
@@ -14,12 +14,15 @@
 
 const average = (arr) => {
   let sum = 0;
+  if (arr.length === 0) {
+    return undefined;
+  }
   for (let item = 0; item < arr.length; item += 1){
     if (typeof arr[item] !== 'number') {
       return undefined;
     } else {sum += arr[item]}
-  return Math.round(sum / arr.length);
   }
+  return Math.round(sum / arr.length);
 };
 
 module.exports = average;
