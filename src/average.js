a52f8570533fee70a31996c301f3122a864ff595
pauloricardoz <trybe.przt@gmail.com>
2020-06-15 15:40:51 -0300

    average done --codeclime 1

--- src/average.js
+++ src/average.js
@@ -15,7 +15,7 @@
 const average = (arr) => {
   if (arr.length === 0) return undefined;
   let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i += 1) {
     if (typeof arr[i] !== 'number') return undefined;
     sum += arr[i];
   }
