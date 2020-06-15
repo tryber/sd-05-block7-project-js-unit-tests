190e3e2f426cd514156d3913e770f0e740e41f92
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-06-15 17:43:35 -0300

    inclusão de condição que define o retorno para array vazio

--- src/average.js
+++ src/average.js
@@ -13,6 +13,9 @@
 */
 
 const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
   let sum = 0;
   for (let i = 0; i < array.length; i += 1) {
     if (typeof array[i] !== 'number') {
