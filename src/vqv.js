6e47b2342c4e8f41d81f706b2ccd71d8b0e2020b
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-06-15 18:40:08 -0300

    Função do arquivo vqv.js DONE!

--- src/vqv.js
+++ src/vqv.js
@@ -15,7 +15,15 @@
       trabalho na Trybe e mando muito em programação!
       #VQV!'
 */
-
-const vqv = (nome, idade) => {};
-
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  } else {
+    return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`
+  };
+};
+console.log(vqv());
 module.exports = vqv;
