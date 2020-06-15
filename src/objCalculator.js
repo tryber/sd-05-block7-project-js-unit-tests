9e4657c00bc1da16fe81b44ea111325a7736cd3f
rfreitasbatista <rfreitasbatista@gmail.com>
2020-06-15 18:27:39 -0300

    Math floor adicionado à calculadora

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,9 +20,9 @@
 */
 
 const calculator = {
-  add: (a, b) => a + b,
-  mult: (a, b) => a * b,
-  div: (a, b) => a / b,
-  sub: (a, b) => a - b,
+  add: (a, b) => Math.floor(a + b),
+  mult: (a, b) => Math.floor(a * b),
+  div: (a, b) => Math.floor(a / b),
+  sub: (a, b) => Math.floor(a - b),
 };
 module.exports = calculator;
