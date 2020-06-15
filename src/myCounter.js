86ae1d7e3473f91ddb38757769d35d95996dff17
rfreitasbatista <rfreitasbatista@gmail.com>
2020-06-15 18:21:32 -0300

    myCounter e objCalculator iniciados

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,11 +19,10 @@ const myCounter = () => {
   const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter1 = 2; counter1 <= 3; counter1 += 1) {
+      myArray.push(counter1);
     }
   }
   return myArray;
 };
-
 module.exports = myCounter;
