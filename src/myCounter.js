f5217b1b532171b1be427e7de5ef1b7a5af71287
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-06-15 18:44:56 -0300

    proper sintax assigned

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,11 +16,11 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
+  const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let i = 2; i <= 3; i += 1) {
+      myArray.push(i);
     }
   }
   return myArray;
