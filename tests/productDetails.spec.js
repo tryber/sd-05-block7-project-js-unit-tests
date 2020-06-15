891f14008dd3591e4f7a24fa32e950bb808c0829
luiz-avelar <luiz.avelar25@gmail.com>
2020-06-15 14:48:52 -0300

    productDetail test

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -36,9 +36,20 @@ describe('#productDetails', () => {
     assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.deepStrictEqual(typeof productDetails('Alcool gel', 'Máscara'), 'array');
+
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
+
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof productDetails('Alcool gel', 'Máscara')[0], 'object');
+    assert.deepStrictEqual(typeof productDetails('Alcool gel', 'Máscara')[1], 'object');
+
     // Teste que os dois objetos são diferentes entre si.
+    assert.deepStrictEqual(productDetails('Alcool gel', 'Máscara')[0] === productDetails('Alcool gel', 'Máscara')[1], false);
+
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara')[0]['details']['productId'].endsWith('123'), true);
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara')[1]['details']['productId'].endsWith('123'), true);
   });
 });
