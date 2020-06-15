af94bcb12d96c8565c786c65e23b2c3922a2022f
hleoc <informig@gmail.com>
2020-06-15 18:22:02 -0300

    Primeiro exercicio - teste 3

--- src/average.js
+++ src/average.js
@@ -19,7 +19,7 @@ const average = (array) => {
   if (array.length === 0) {
     return undefined;
   }
-  for (const i in array) {
+  for (let i = 0; i < array.length; i += 1) {
     if (typeof array[i] !== 'number') {
       return undefined;
     }
