69476d7ad3e223bd0ec60d000aee00b1c9f573aa
Lucas Costa Ventura <lucascv333@gmail.com>
2020-06-15 15:58:49 -0300

    add src/average.js

--- src/average.js
+++ src/average.js
@@ -12,6 +12,17 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (arr) => {
+  if (arr.length === 0) return undefined;
+  
+  let soma = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (typeof(arr[i]) !== 'number') {
+      return undefined;
+    }
+    soma += arr[i];
+  }
+  return -Math.round(-soma / array.length);  
+};
 
 module.exports = average;
