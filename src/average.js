77b77a10d0d922e82fe738d7eec2a3705f856cae
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-06-15 17:30:38 -0300

    Requisito numero 1 DONE

--- src/average.js
+++ src/average.js
@@ -12,6 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+
+const average = (array) => {
+  if(array.length == 0){
+    return undefined;
+  }
+  let resultado = 0;
+  for(let i = 0; i < array.length; i+=1){
+    if(typeof array[i] !== 'number'){
+      return undefined;
+    }
+    resultado += array[i];
+  } 
+  return Math.round(resultado/array.length)
+};
 
 module.exports = average;
+
+console.log(average ([1, 2, 3]))
