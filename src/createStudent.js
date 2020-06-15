30faf42378d7d49238dcf17f8d3857e3629ad11b
Lucas Costa Ventura <lucascv333@gmail.com>
2020-06-15 18:38:13 -0300

    corrigindo frase do createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,7 +18,7 @@
 const createStudent = (nome) => {
   const estudante = {
     name: nome,
-    feedback: () => 'Eite pessoa boa!',
+    feedback: () => 'Eita pessoa boa!',
   };
   return estudante;
 };
