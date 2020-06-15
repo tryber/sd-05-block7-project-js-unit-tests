1c2495329f80037ef20e07725e80983a91871a40
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-06-15 16:40:44 -0300

    ex06 done

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,20 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
+    const productDefault = productDetails('Alcool gel', 'Máscara');
+
     // Teste que o retorno da função é um array.
+    assert.ok(Array.isArray(productDefault));
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.equal(productDefault.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.ok(productDefault.every((i) => typeof(i) === 'object'));
     // Teste que os dois objetos são diferentes entre si.
+    assert.ok(productDefault[0] != productDefault[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.ok(productDefault[0].details.productId.endsWith('123'));
+    assert.ok(productDefault[1].details.productId.endsWith('123'));
   });
 });
