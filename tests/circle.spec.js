92c979e5d9b424c34bc08ab8ac718230bd18dfb3
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-06-15 17:52:22 -0300

    circle refatoraçao

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -38,6 +38,6 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     assert.strictEqual(Object.values(circle(3))[1], 28.259999999999998);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.equal(Object.values(circle(3)), [3, 28.259999999999998, 18.84]);
+    assert.deepStrictEqual(circle(3), {radius: 3, area: 28.259999999999998, circumference: 18.84});
   });
 });
