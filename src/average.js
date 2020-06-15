93723d72cc91dc93b80c40637818b57bff7dc56c
Thiago <asm.thiago@gmail.com>
2020-06-15 19:23:01 -0300

    Add Function

--- src/average.js
+++ src/average.js
@@ -12,6 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (arr) => {
+  if (arr.length === 0) {
+    return undefined; 
+  }
 
+  let sum = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (typeof arr[i] !== `number`) {
+      return undefined;
+    }
+    sum += arr[i];
+  }
+  return Math.round(sum / arr.length)
+};
 module.exports = average;
