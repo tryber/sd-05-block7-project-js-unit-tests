1c2de757bf3f54031de20767922a96137b6f27d9
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-06-15 18:05:19 -0300

    Refatoraçao product details

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -36,7 +36,7 @@ describe('#productDetails', () => {
     //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    assert.strictEqual(typeof productDetails('teste1', 'teste2'), 'array');
+    assert.strictEqual(Array.isArray(productDetails('teste1', 'teste2')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(productDetails('teste1', 'teste2').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
