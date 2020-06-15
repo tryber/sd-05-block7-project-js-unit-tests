7954216e295715fb2989ad8dd95e5ffd48ef63a0
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-06-15 14:59:16 -0300

    part I done

--- src/average.js
+++ src/average.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 /*
   A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
   Caso a função receba algum valor não númerico ou um array vazio,
@@ -12,6 +13,17 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  const length = array.length;
+  if(!array.every((n) => Number.isInteger(n))) return undefined;
+  const sum = array.reduce((total, n) => total + n);
+  return Math.floor(sum/length);
+};
+
+
+assert.strictEqual(typeof(average), 'function');
+assert.strictEqual(average([2, 2]), 2);
+assert.strictEqual(average([1, 2]), 1);
+assert.strictEqual(average([2, '2']), undefined);
 
 module.exports = average;
