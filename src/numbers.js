62d5a42ece0cf92fac7702c64da3acb2db20afc9
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-06-15 15:56:04 -0300

    erros sem explicacao

--- src/numbers.js
+++ src/numbers.js
@@ -19,3 +19,7 @@ const numbers = (myArray) => {
 };
 
 module.exports = numbers;
+const assert = require('assert');
+assert.strictEqual(numbers([1, 2, 3, 4, 5]), true)
+assert.strictEqual(numbers([1, 2, '3', 4, 5]), false)
+assert.strictEqual(numbers([1, 'a', 3]), false)
