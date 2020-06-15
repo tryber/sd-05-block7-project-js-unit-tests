28c789f927c5f7cb8ba545bcffa448f82333a4c2
Lucas Costa Ventura <lucascv333@gmail.com>
2020-06-15 16:02:52 -0300

    corrigin code climate

--- src/average.js
+++ src/average.js
@@ -14,15 +14,14 @@
 
 const average = (arr) => {
   if (arr.length === 0) return undefined;
-  
   let soma = 0;
   for (let i = 0; i < arr.length; i += 1) {
-    if (typeof(arr[i]) !== 'number') {
+    if (typeof arr[i] !== 'number') {
       return undefined;
     }
     soma += arr[i];
   }
-  return -Math.round(-soma / array.length);  
+  return -Math.round(-soma / array.length);
 };
 
 module.exports = average;
