8d8343a1cbb86f6a6e4da115b8a7b7da832d6845
matambrosio <matmagamb1@gmail.com>
2020-06-15 19:01:13 -0300

    9-commit

--- src/average.js
+++ src/average.js
@@ -14,19 +14,23 @@
 
 const average = (number) => {
   let Retorno = 0;
+  if (number.length === 0) {
+    console.log('undefined');
+    return (undefined);
+  }
   for (let i = 0; i < number.length; i += 1) {
-    if (number.length === 0 || number[i] !== (number[i] + 0)) {
-      return (undefined);
-    } else if (number[i] === (number[i] + 0)) {
+    if (number[i] === (number[i] + 0)) {
       Retorno += number[i];
+    } else {
+      return (undefined);
     }
   }
   console.log(Math.round(Retorno / number.length));
   return (Math.round(Retorno / number.length));
 };
 /*
-  let arr = [];
-  average(arr);
+let arr = [1,2,3,4,5,6];
+console.log(average(arr));
 */
 
 module.exports = average;
