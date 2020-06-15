7ddc931d96399129f1af7c4eaeefb2fd427199ba
RenataNovais <rnovaiscs@gmail.com>
2020-06-15 19:18:06 -0300

    fixing CodeClimate - requirement 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -16,9 +16,9 @@
 */
 
 const createStudent = (name) => {
-  let estudante = {
-  name: name,
-  feedback: () => 'Eita pessoa boa!'
+  const estudante = {
+  name,
+  feedback: () => 'Eita pessoa boa!',
   }
   return estudante;
 };
