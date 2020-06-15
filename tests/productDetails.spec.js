24daa8181f3f106ba237c09aea643ca1581947d6
rfreitasbatista <rfreitasbatista@gmail.com>
2020-06-15 19:13:51 -0300

    Excluding console.log

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -35,9 +35,17 @@ describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
     assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
+    assert.equal(typeof productDetails('Alcool gel', 'Máscara'), 'array')
     // Teste que o retorno da função é um array.
+    assert.equal((productDetails('Alcool gel', 'Máscara')).length, 2)
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.deepEqual(productDetails('Alcool gel', 'Máscara'), [{name: 'Alcool gel', details: {
+      productId: 'Acool gel123'
+    }},{name: 'Máscara', details: {
+      productId: 'Máscara123'
+    }}])
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    
     // Teste que os dois objetos são diferentes entre si.
     // (Difícil) Teste que os dois productIds terminam com 123.
   });
