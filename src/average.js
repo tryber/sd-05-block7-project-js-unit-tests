bd19c8b2377d92309225fd7b2dbdb5fc9766e138
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-06-15 14:58:40 -0300

    Commit inicial, implementaçao da funçao average

--- src/average.js
+++ src/average.js
@@ -12,6 +12,17 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let soma = 0;
+  for (let i = 0; i < array.length; i += 1){
+    if (isNaN(array[i])) {
+      return undefined;
+    } else {
+      soma += Math.round(array[i]);
+    }
+  }
+  let media = soma / array.length;
+  return media;
+};
 
 module.exports = average;
