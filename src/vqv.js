fea79e03d70088e7dced6283aff1a503fb3f3d7d
luiz-avelar <luiz.avelar25@gmail.com>
2020-06-15 15:06:47 -0300

    Vqv function correction

--- src/vqv.js
+++ src/vqv.js
@@ -19,11 +19,13 @@
 const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) return undefined;
 
-  const output = `Oi, meu nome é ${nome}!
-               tenho ${idade} anos,
-               trabalho na Trybe e mando muito em programação!
-               #VQV!`;
+  const output =
+`Oi, meu nome é ${nome}!
+tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+
   return output;
 };
-
+console.log(vqv('Tunico', 29));
 module.exports = vqv;
