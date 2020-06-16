d416ec5965de6916fe298c55772e9c91ba4e45fa
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-06-16 11:32:53 -0300

    Função do arquivo createStudent.js DONE!

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,16 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (nome) => {
+  const objeto = {
+    name: nome,
+    feedback: () => {
+      return 'Eita pessoa boa!'
+    }
+  }
+  return objeto
+};
+const estudante = createStudent('Leandrão, o Lobo Solitário')
+console.log(estudante.name)
+console.log(estudante.feedback())
 module.exports = createStudent;
