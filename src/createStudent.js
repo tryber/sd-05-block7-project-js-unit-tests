4637477fc5b63e198d085e9a0f5906dcba38efd4
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-06-16 12:01:07 -0300

    Fixing codeclimate

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,9 +18,7 @@
 const createStudent = (nome) => {
   const objeto = {
     name: nome,
-    feedback: () => {
-      return 'Eita pessoa boa!';
-    }
+    feedback: () => 'Eita pessoa boa!'
   };
   return objeto;
 };
