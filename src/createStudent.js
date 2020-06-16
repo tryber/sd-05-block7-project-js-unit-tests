920df8df59b55ae52dbd093851114a6bc522b8af
LucasStaroscky <staroscky60@gmail.com>
2020-06-15 22:54:38 -0300

    codeclimate errors fixed in the sixth requirement

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,7 +18,7 @@
 const createStudent = (string) => {
   const estudante = {
     name: string,
-    feedback: () => 'Eita pessoa boa!'
+    feedback: () => 'Eita pessoa boa!',
   };
   return estudante;
 };
