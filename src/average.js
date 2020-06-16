7bb85c7faa6d40ceffd19eb4db9f7e0e1b5f59f1
Cass <thecastieel07@gmail.com>
2020-06-15 23:10:08 -0300

    finalized..

--- src/average.js
+++ src/average.js
@@ -12,21 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 const average = (arr) => {
-  if(arr.length === 0){
-    return undefined
+  if (arr.length === 0) {
+    return undefined;
   }
   let soma = 0;
-  for(let i  = 0;  i < arr.length; i ++  ){
-    if (typeof arr[i] != 'number'){
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] != "number") {
       return undefined;
-
-
     }
     //isNaN(arr[i])
-      soma = soma + arr[i]
-  }  
-  return Math.round(soma / arr.length) 
+    soma = soma + arr[i];
+  }
+  return Math.round(soma / arr.length);
 };
 
 module.exports = average;
-
