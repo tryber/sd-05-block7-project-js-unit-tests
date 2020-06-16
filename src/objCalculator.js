1ee9a2abaddafd23798a8c94f0b4e8b0fc71abe2
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-06-15 22:39:11 -0300

    Implementacao da funcao objCalculator

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -5,20 +5,25 @@
     - div;
     - sub.
   Para cada uma delas atribua uma função que realiza a respectiva operação.
-  A função deve receber dois inteiros e retornar um inteiro.
+  A função deve receber dois numABs e retornar um numAB.
   Os resultados das divisões devem sempre ser arredondados para baixo.
 
   Faça as funções com arrow functions!
 
   Parâmetros:
-  - Um número inteiro;
-  - Um número inteiro;
+  - Um número numAB;
+  - Um número numAB;
 
   Comportamento:
   calculator.add(1, 1) // Retorno: 2;
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+    add: (numA, numAB) => Math.round(numA + numAB),
+    mult: (numA, numAB) => Math.round(numA * numAB),
+    div: (numA, numAB) => Math.floor(numA / numAB),
+    sub: (numA, numAB) => Math.round(numA - numAB),
+};
 
 module.exports = calculator;
