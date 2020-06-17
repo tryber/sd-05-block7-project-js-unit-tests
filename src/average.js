1e3abe7ee6cbcad1ff2d6c0c172ba59d33814454
Sidnei Ramos <sidneiramos02@gmail.com>
2020-06-17 00:54:56 -0300

    Arrumando Climate 2

--- src/average.js
+++ src/average.js
@@ -15,7 +15,7 @@
 const average = (array) => {
   let sum = 0;
   if (!array.length) return undefined;
-  for (i = 0; i < array.length ; i += 1) {
+  for (i = 0; i < array.length; i += 1) {
     if (typeof array[i] !== 'number') {
       return undefined;
     }
