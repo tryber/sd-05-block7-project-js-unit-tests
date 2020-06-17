ab9e8b6cde501ac5e8070437bc67aa3c7107d19e
hleoc <informig@gmail.com>
2020-06-16 22:47:29 -0300

    Quinto exercício

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,4 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
+const createStudent = (nome) => {
+  const student = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return student;
+};
+
 module.exports = createStudent;
