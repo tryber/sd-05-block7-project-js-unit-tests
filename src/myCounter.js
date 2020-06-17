e95cb094796c7c23d147bcee344edd9b109b73bd
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-06-16 22:03:24 -0300

    Fixing codeclimate

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,13 +16,13 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
+  const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
       myArray.push(counter2);
-    };
-  };
+    }
+  }
   return myArray;
 };
 console.log(myCounter());
