5075ec4d146239137cbed6d8ac6caf1a1158b743
Elisa <elisatcamillo@gmail.com>
2020-06-18 16:37:29 -0300

    6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -3,6 +3,7 @@
 
 const assert = require('assert');
 const productDetails = require('../src/productDetails');
+const { type } = require('os');
 
 /*
   Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
@@ -33,12 +34,17 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.ok(Array.isArray(productDetails()))
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails.length, 2)
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.ok(productDetails().every((obj) => typeof obj === 'object' ))
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(productDetails('Teste1')[0], productDetails('Teste2')[1])
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.ok(productDetails('Teste1')[0].details.productId.endsWith('123') && productDetails('Teste2')[1].details.productId.endsWith('123'));
   });
 });
