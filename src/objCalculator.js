ab90c16a42f6d61424b9cf495cd8416a44cfc9cc
Larissa Palombo <larissampalombo@gmail.com>
2020-06-19 18:01:13 -0300

    tudo ok

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -22,7 +22,7 @@
 const calculator = {
   add: (n1, n2) => Math.round(n1 + n2),
   mult: (n1, n2) => Math.round(n1 * n2),
-  div: (n1, n2) => Math.floor(n1  / n2),
+  div: (n1, n2) => Math.floor(n1 / n2),
   sub: (n1, n2) => Math.round(n1 - n2),
 };
 
