80d41cc25f13448a1b3f6a4dbaab9ed63f51488c
ferrari <luisdiasferrari@gmail.com>
2020-06-19 01:37:11 -0300

    requirement ex06

--- src/productDetails.js
+++ src/productDetails.js
@@ -39,6 +39,4 @@ const productDetails = (firstProduct, secondProduct) => [
   },
 ];
 
-console.log((productDetails('Alcool gel', 'Máscara')[0].details.productId).substr(-3))
-
 module.exports = productDetails;
