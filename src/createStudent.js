ce0ee9ff0ee48930b4b30fc5f98316532ee011a5
Larissa Palombo <larissampalombo@gmail.com>
2020-06-19 17:10:46 -0300

    student ok

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const print = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  }
+  return print;
+};
 
 module.exports = createStudent;
