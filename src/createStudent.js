894fb51a26a23d8229c920519d1d85d3d73f9bc7
luialbeto <araujo.script@gmail.com>
2020-06-19 11:35:20 -0300

    implementando função createStudents.js

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = nome => {
+  name: 'Leandrão, o Lobo Solitário'
+  feedback: () => 'Eita pessoa boa!'
+};
 
 module.exports = createStudent;
