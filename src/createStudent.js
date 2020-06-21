5875efaed8641855caea238f45cc93481321aba0
luialbeto <araujo.script@gmail.com>
2020-06-21 15:33:35 -0300

    correção climate createStudents.js

--- src/createStudent.js
+++ src/createStudent.js
@@ -17,7 +17,7 @@
 
 const createStudent = (nomeEstudante) => {
   const object = {
-  `${Leandrão, o Lobo Solitário}`
+  '${Leandrão, o Lobo Solitário}'
   feedback() => ('Eita pessoa boa!')
 };
 return object;
