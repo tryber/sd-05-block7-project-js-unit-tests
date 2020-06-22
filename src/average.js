73c0550e27ed03b141880aa8e65858b406b7415d
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-06-21 22:16:11 -0300

    Primeiro Commit

--- src/average.js
+++ src/average.js
@@ -13,13 +13,13 @@
 */
 
 const average = (array) => {
-  if(array.length ===0) return undefined
-  let soma = 0;
+  if (array.length === 0) return undefined;
+  let sum = 0;
   for (i = 0; i < array.length; i += 1) {
-    if (typeof array[i]!== 'number') return undefined
-    soma += array[i];
+    if (typeof array[i] !== 'number') return undefined;
+    sum += array[i];
   }
-  return Math.round(soma / array.length);
+  return Math.round(sum / array.length);
 };
 
 module.exports = average;
