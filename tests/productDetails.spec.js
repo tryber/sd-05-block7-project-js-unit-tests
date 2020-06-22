1d5f02d623724b429fb5c1fb1f31d860fc442a04
Cass <thecastieel07@gmail.com>
2020-06-21 22:37:09 -0300

    atualized

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,8 +1,8 @@
 /* eslint-disable max-len*/
 /* eslint-disable no-unused-vars */
 
-const assert = require("assert");
-const productDetails = require("../src/productDetails");
+const assert = require('assert');
+const productDetails = require('../src/productDetails');
 
 /*
   Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
@@ -31,22 +31,37 @@ const productDetails = require("../src/productDetails");
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe("#productDetails", () => {
-  it("tests the function has the correct behaviour", () => {
+describe('#productDetails', () => {
+  it('tests the function has the correct behaviour', () => {
     // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    assert.strictEqual(typeof productDetails(3), "object");
+    assert.strictEqual(typeof productDetails(3), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
-    assert.strictEqual(productDetails(2, "will").length, 2);
+    assert.strictEqual(productDetails( 2, 'Nanazinha').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+
     assert.deepStrictEqual(
       [typeof productDetails(2, 1)[0], typeof productDetails(2, 1)[1]],
-      ["object", "object"]
+      ['object', 'object']
     );
 
     // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(
+      productDetails(1, 2)[0] !== productDetails(1, 2)[0],
+      true
+    );
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual(
+      [
+        /123$/.test(
+          productDetails('alcool', 'detergente')[0].details.productId
+        ),
+        /123$/.test(
+          productDetails('alcool', 'detergente')[1].details.productId
+        ),
+      ],
+      [true, true]
+    );
   });
 });
-// assert.stricEqual(FUNCAO, RESPOSTA CORRETA )
