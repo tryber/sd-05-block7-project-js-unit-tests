701a366a414551c945cf2f6e40e02130d04991d6
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-06-22 02:12:11 -0300

    myCounter... me enganei no ultimo commit. esse era o mais fácil.

--- src/vqv.js
+++ src/vqv.js
@@ -17,11 +17,12 @@
 */
 
 const vqv = (nome, idade) => {
-  if(typeof name === 'undefined' || typeof idade === 'undefined') return undefined
+  if (typeof name === 'undefined' || typeof idade === 'undefined')
+    return undefined;
   return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`
+#VQV!`;
 };
 
 module.exports = vqv;
