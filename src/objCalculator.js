d3a0f45675eada3beadae955f4c2420ffd5ed6cf
Cass <thecastieel07@gmail.com>
2020-06-21 23:10:11 -0300

    ObjCalculator atualized

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -1,3 +1,10 @@
+const calculator = {
+  add: (num1, num2) => num1 + num2,
+  mult: (num1, num2) => num1 * num2,
+  div: (num1, num2) => Math.floor(num1 / num2),
+  sub: (num1, num2) => num1 - num2,
+};
+
 /*
   Desenvolva um objeto calculator que possui quatro chaves:
     - add;
@@ -18,7 +25,5 @@
   calculator.add(1, 1) // Retorno: 2;
   calculator.div(3, 2) // Retorno: 1;
 */
-
-const calculator = {};
 
 module.exports = calculator;
