d9e9ae2734f24c43a0ebd720b5c77ab771cda146
luca <luucadecastro@gmail.com>
2020-06-22 13:27:34 -0300

    codeclimate 3

--- src/vqv.js
+++ src/vqv.js
@@ -18,7 +18,7 @@
 
 const vqv = (nome, idade) => {
   if (typeof nome === 'undefined' || typeof idade === 'undefined') {
-  return undefined;
+    return undefined;
   }
   return `Oi, meu nome é ${nome}!\n` +
   `Tenho ${idade} anos,\n` +
